c2f359bf63500bbf54d610f3775b25360d3644cc
Matt Burke <Burke9077@gmail.com>
2023-11-26 16:36:21 -0500

    Socket.io communication working

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const fs = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
 const { engine } = require('express-handlebars');
+const moment = require('moment');
 
 // Load the application wide configuration file
 const configPath = path.join(__dirname, 'config.yml');
@@ -40,14 +41,11 @@ app.set('view engine', 'hbs');
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
-// Configure livereload
-const livereload = require('livereload');
-const connectLivereload = require('connect-livereload');
-const liveReloadServer = livereload.createServer();
-liveReloadServer.watch(__dirname, "views");
-liveReloadServer.watch(__dirname, "public");
-app.use(connectLivereload());
-
+// Create HTTP server and integrate with socket.io
+const http = require('http');
+const { Server } = require('socket.io');
+const server = http.createServer(app);
+const io = new Server(server);
 
 /*
     InfluxDB setup and initial data load
@@ -104,12 +102,22 @@ app.get('/data-trends', (req, res) => {
 	modifiedChartConfig.variablegroups.forEach(group => {
 		group.isSelected = (group.name === "All Variables");
 	});
-	
+
+	/* 
+		Our datepickers need reasonable defaults, what's the current date and the date 
+		from 3 days ago (formatted yyyy-MM-dd) that we can send in as defaults?
+	*/
+	let defaultSearchRange = {
+		threeDaysAgo: moment().subtract(3, 'days').format('YYYY-MM-DD'),
+		currentDate: moment().format('YYYY-MM-DD')
+	};
+		
 	res.render('data-trends', {
 		config: config,
 		chartconfig: modifiedChartConfig,
 		navbar: {datatrends: true},
 		pageName: 'Data Trends',
+		defaultSearchRange: defaultSearchRange
 	});
 });
 
@@ -118,6 +126,34 @@ app.get('/data-trends', (req, res) => {
 	Finally, start the web server
 */
 
-app.listen(port, hostname, () => {
+server.listen(port, hostname, () => {
 	console.log(`Server is running on http://${hostname}:${port}`);
+});
+
+// Configure our socket.io server for live events and updating
+io.on('connection', (socket) => {
+    console.log('A socket.io user connected');
+    socket.on('disconnect', () => {
+        console.log('A socket.io user disconnected');
+    });
+
+    /*
+		Listen for data requests from users
+	*/
+
+	// Request a historical (date & time range) chart
+    socket.on('historical-chart', (data) => {
+		let dataf = JSON.stringify(data, null, 4);
+		console.log(`Historical chart requested: ${dataf}`);
+        // Process the data and emit a response
+        io.emit('some response', { some: 'data' });
+    });
+
+	// Request a relative (live) chart
+    socket.on('relative-chart', (data) => {
+		let dataf = JSON.stringify(data, null, 4);
+		console.log(`Relative chart requested: ${dataf}`);
+        // Process the data and emit a response
+        io.emit('some response', { some: 'data' });
+    });
 });
